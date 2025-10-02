601f97782d25c429c74450b93a87a6f7bb1fd5a1
abdulmalick42 <abdulmalick6330@gmail.com>
2025-10-02 23:18:35 +0530

    added text

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MergeHome extends StatefulWidget {
 }
 
 class _MergeHomeState extends State<MergeHome> {
-  String title = 'Hello number2';
+  String title = 'Hello number3';
 
   int counter = 0;
 
